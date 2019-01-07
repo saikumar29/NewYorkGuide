830f628c1b9e69d94dfd607296f93fc8693aa2ed
Saikumar Katepalli <katepalli.s@husky.neu.edu>
2019-01-07 14:10:02 -0500

    INFO-6150_NewYorkGuide Project

---
+++ newyorkguide/models/vouchers.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//Create Schema
+
+const VoucherSchema =  new Schema({
+    
+    vouchername:{
+        type: String    
+    },
+    price:{
+        type: String
+    },
+    description: { 
+        type: String
+    }
+});
+
+mongoose.model('products', VoucherSchema);
